29f9afea4116eca4200f0c86747edf1c3265ad18
Maksim Ananev <m.ananev@andersenlab.com>
2022-05-16 13:12:50 +0500

    initial commit

---
+++ src/test/java/com/example/mentorsstudents/MentorsStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorsstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorsStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
